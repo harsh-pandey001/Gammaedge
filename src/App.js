f2ed061315d77b49aea163401ded2cd2ae07cc0f
Harsh <harshpandey2060@gmail.com>
2024-03-12 12:03:06 +0530

    Project initiated

--- src/App.js
+++ src/App.js
@@ -8,16 +8,13 @@ import {
   Routes
 } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import List from "./Pages/list/List";
-import Turf from "./Pages/Turf/Turf";
+
 
 function App() {
   return (<>
   <BrowserRouter>
   <Routes>
     <Route path = "/" element = {<Home/>}/>
-    <Route path = "/turfs" element = {<List/>}/>
-    <Route path = "/Turf:id" element = {<Turf/>}/>
   </Routes>
   </BrowserRouter>
   </>)
