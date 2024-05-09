e0b241c9a238b29eaae783120034d5b895c83589
Harsh <harshpandey2060@gmail.com>
2024-05-09 16:24:40 +0530

    imporved folder structure

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./connection/db");
+const router = require("./routes/formRoutes");
+const app = express();
+const port = 5000;
+app.use(express.json());
+
+connectDB();
+app.use(
+  cors({
+    origin: ["https://my-indore-guide-fontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.use("/form/signup", router);
+app.use("/form/auth", router);
+app.use("/form/data", router);
+// payment Routes
+// app.use("/payment",paymentRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
